83c7620fa4913b4fd3632c9a95824c68a1b900f4
skkkk98 <salgu1998@gmail.com>
2021-10-14 13:01:30 +0900

    first commit

---
+++ src/main/java/com/pirates/pirates_server/PiratesServerApplication.java
@@ -0,0 +1,13 @@
+package com.pirates.pirates_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiratesServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiratesServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pirates/pirates_server/PiratesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pirates.pirates_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiratesServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
